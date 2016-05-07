f432820bc58f882bb92ffc8a3ca46f8db4563e20
fangbian <156382880@qq.com>
2016-05-07 15:04:35 +0800

    1
    1

---
+++ liutiansi_P13_2.py
@@ -0,0 +1,10 @@
+import dns.resolver
+domain=raw_input('please input a domain:')
+A=dns.resolver.query(domain,'A')
+for i in A.response.answer:
+	for j in i.items:
+		print j.address
+	
+#MX=dns.resolver.query(domain,'MX')
+#for i in MX:
+#	print 'MX preference=',i.preference, 'mail exchanger=', i.exchange
